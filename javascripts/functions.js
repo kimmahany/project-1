a5417aebf7c4e9b342987ccf8fbf3a7b5bd78236
Kim Mahany <kmahany@uoregon.edu>
2020-04-09 17:09:03 -0700

    Add arrAvg to functions.js

---
+++ javascripts/functions.js
@@ -0,0 +1,16 @@
+// jshint esversion: 7
+
+//Req. 3B
+let arrAvg = arr => {
+  let sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum / arr.length;
+};
+
+//Req. 3C
+let arrMax = arr => {};
+
+// Req. 3D
+let sumEvens = arr => {};
